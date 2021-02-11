73dc0a1fdd818f302a609f5292e9cab6d80caef0
huskyhoochu <dfg1499@gmail.com>
2021-02-11 19:08:29 +0900

    Add history posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import Layout from '@src/layouts';
+import { graphql, PageProps, Link } from 'gatsby';
 
-const IndexPage: React.FC = () => {
+const IndexPage: React.FC = ({ data }: PageProps) => {
+  const posts = (data as any).allMarkdownRemark;
   return (
     <Layout>
-      <p>hello</p>
+      <p>{posts.totalCount} posts</p>
+      {Array.prototype.map.call(posts.edges, ({ node }) => (
+        <div key={node.id}>
+          <Link to={node.frontmatter.slug}>
+            <p>{node.frontmatter.title}</p>
+            <p>{node.frontmatter.date}</p>
+          </Link>
+        </div>
+      ))}
     </Layout>
   );
 };
+
+export const query = graphql`
+  query {
+      allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+          totalCount
+          edges {
+              node {
+                  id
+                  frontmatter {
+                      title
+                      slug
+                      date
+                  }
+              }
+          }
+      }
+  }
+`;
 
 export default IndexPage;
