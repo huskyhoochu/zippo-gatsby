6dcfc3e199addec2f948be077a27a644d31c92ce
huskyhoochu <dfg1499@gmail.com>
2021-02-14 12:56:37 +0900

    Style list

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -20,15 +20,14 @@ const Card: React.FC<Props> = ({ node }: Props) => {
   return (
     <div className="card">
       <Link to={node.frontmatter.slug}>
+        <div className="card__thumbnail" style={{ backgroundImage: `url(${node.frontmatter.thumbnail})` }} />
         <div className="card__wrapper">
-          <div className="card__thumbnail" style={{ backgroundImage: `url(${node.frontmatter.thumbnail})` }} />
           <div className="card__tag-group">
             <p className="capitalize">{node.frontmatter.tag}</p>
             <p>{node.frontmatter.date}</p>
           </div>
           <h3 className="card__title">{node.frontmatter.title}</h3>
         </div>
-
       </Link>
     </div>
   );
